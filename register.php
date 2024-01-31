f0264830019b359a15658dc8ad62be14c4ba5bf7
Souleymane <150073079+Souley20@users.noreply.github.com>
2024-01-31 10:56:46 +0100

    first commit

---
+++ register.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta> name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inscription</title>
+</head>
+<body>
+  <h1>Inscription</h1>
+  <form action="register.php" method="POST">
+    <label for="name">Nom : </label>
+    <input type="text" name="name" required/><br><br>
+
+    <label for="surname">Prenom : </label>
+    <input type="test" name="surname"/><br><br>
+
+    <label for="pseudo">Pseudo : </label>
+    <input type="test" name="pseudo"/><br><br>
+
+    <label for="email">Adresse email : </label>
+    <input type="email" name="email" required/><br><br>
+
+    <label for="password">Mot de passe : </label>
+    <input type="password" name="password" required/><br><br>
+
+    <input type="submit" value="S’inscrire">
+  </form>
+</body>
+</html>
